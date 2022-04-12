af581f8332c6984ab14fca95ad73f7668fb2415b
Ahmed-Amine-Hlel <kirigaya.kazuto.hlel@gmail.com>
2022-04-12 20:03:33 +0100

    Spring Boot : Spring MVC Coté Serveur
    Spring MVC Coté Serveur :
    Thymeleaf et Bootstrap

---
+++ produits/src/main/java/com/ahmed/produits/ProduitsApplication.java
@@ -0,0 +1,26 @@
+package com.ahmed.produits;
+
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.ahmed.produits.entities.Produit;
+import com.ahmed.produits.service.ProduitService;
+
+@SpringBootApplication
+public class ProduitsApplication implements CommandLineRunner {
+@Autowired
+ProduitService produitService;
+public static void main(String[] args) {
+SpringApplication.run(ProduitsApplication.class, args);
+}
+
+
+@Override
+public void run(String... args) throws Exception {
+produitService.saveProduit(new Produit("Clavier MSI", 260.0, new Date()));
+produitService.saveProduit(new Produit("IPAD Apple", 4500.0, new Date()));
+produitService.saveProduit(new Produit("Imprimante Thomson", 900.0, new Date()));
+}
+}
